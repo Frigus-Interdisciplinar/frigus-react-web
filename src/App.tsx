6056fbf9cf9b0c4b57a948c5e8b7b201b9b5ce29
gabriel03013 <gm5770283@gmail.com>
2026-05-30 19:25:20 -0300

    test: change app.tsx to test the api function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { api } from "./utils/api.util";
+
+// apenas para teste da funcao, sera removido
+
+type Response = {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: {rate: number, count: number }, 
+}
+
 const App = () => {
-  return <div className="bg-[#2e2e2e]">App</div>;
+  const [data, setData] = useState<Response[]>([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await api<Response[]>("/products", "GET");
+      setData(res);
+    }
+    loadData();
+  })
+
+  return <div className="bg-[#2e2e2e]">
+    <ul>
+      {data && data.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+    
+  </div>;
 };
 
 export default App;
