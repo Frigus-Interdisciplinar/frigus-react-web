8417ee6f3265274fab9faeaeb06dc454ba2620b9
gabriel03013 <gm5770283@gmail.com>
2026-08-24 18:17:35 -0300

    feat(auth): add login and register service requests and refine schema validation

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.email(),
-  password: z.string().min(8),
+  email: z.email("Email inválido"),
+  password: z.string("Senha inválida").min(8, "Senha inválida"),
 });
 
 export const registerSchema = z.object({
