0e513edacf8acc2ff622c46973d30da7fced54f5
gabriel03013 <gm5770283@gmail.com>
2026-08-24 18:17:44 -0300

    feat(auth): implement initial login page layout and form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
-import { useState } from "react";
-import Sidebar, { type SectionString } from "@/components/Sidebar/Navbar";
-import Button from "@/components/Button";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
-  const [activeSection, setActiveSection] = useState<SectionString>("home");
-
-  return (
-    <div className="flex min-h-screen bg-bg-app">
-      <Sidebar activeSection={activeSection} />
-
-      <main className="flex-1 p-8">
-        <h1 className="text-2xl font-title text-text-main mb-6">
-          Teste da Sidebar e Componentes
-        </h1>
-
-        <div className="flex flex-wrap gap-4 items-center">
-          <Button variant="primary" onClick={() => setActiveSection("home")}>
-            Visão geral
-          </Button>
-          <Button variant="secondary" onClick={() => setActiveSection("stock")}>
-            Estoque
-          </Button>
-          <Button variant="accent" onClick={() => setActiveSection("recipe")}>
-            Receitas
-          </Button>
-          <Button variant="destructive">
-            Excluir
-          </Button>
-          <Button variant="outline">
-            Configurações
-          </Button>
-        </div>
-      </main>
-    </div>
-  );
+  return <>
+    <LoginPage />
+  </>
 }
-
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,80 @@
+import blueDecorative from "@/assets/blue-decorative.svg";
+import yellowDecorative from "@/assets/yellow-decorative.svg";
+import frigusLogoText from "@/assets/frigus-logo-text.svg";
+
+export default function LoginPage() {
+  return (
+    <main className="flex h-screen w-screen">
+      <section className="w-[610px] bg-frigus-navy relative overflow-hidden pl-[64px]">
+
+        <img src={frigusLogoText} alt="Frigus Logo with text" className="w-[150px] h-[45px] absolute top-[54px] left-[64px]" />
+
+        <div className="flex flex-col gap-[24px] mt-[188px] w-fit">
+          <h1 className="text-[40px] font-bold leading-[46px] text-frigus-white">
+            Menos desperdício <br />
+            Mais leveza no dia a dia
+          </h1>
+
+          <p className="text-[20px] font-medium leading-[27px] font-normal text-frigus-ice">
+            Acompanhe seus alimentos, organize compras e <br />
+            aproveite melhor tudo o que já está em casa
+          </p>
+        </div>
+
+        <img
+          src={blueDecorative}
+          alt="Forma decorativa azul - Blue Decorative"
+          className="w-[195px] h-[390px] absolute bottom-[-20px] right-0 pointer-events-none z-0"
+        />
+        <img
+          src={yellowDecorative}
+          alt="Forma decorativa amarela - Yellow Decorative"
+          className="w-[88px] h-[88px] absolute top-[98px] right-[48px] pointer-events-none z-0"
+        />
+
+        <div className="relative w-[430px] h-[125px] rounded-frigus bg-[#283064] mt-[130px] z-10 flex flex-col gap-2 justify-center pl-[24px]">
+          <h3 className="font-bold text-frigus-white text-[18px]">Sua rotina em um só lugar</h3>
+          <p className="font-normal text-frigus-ice leading-[21px]">
+            Estoque, validade, receitas e lista de compras <br />
+            conectados de forma simples.
+          </p>
+        </div>
+      </section>
+
+      <section className="flex-1 flex items-center justify-center">
+
+        <div className="rounded-frigus bg-frigus-white shadow-[0_16px_34px_0_rgba(19,28,85,0.10)] w-[520px] h-[575px] pl-10 pt-12">
+
+          <h2 className="font-bold text-frigus-navy text-[28px]">Bem-vindo(a) de volta!</h2>
+
+          <p className="font-normal text-[#70809F] text-[16px] mt-1">
+            Entre para continuar cuidando melhor da sua rotina.
+          </p>
+
+          <form className="flex flex-col gap-2 mt-10">
+          <label htmlFor="login-input-email" className="text-blue-950 text-base font-semibold">
+            Email
+          </label>
+          <input type="text" id="login-input-email" className="w-105 h-12 bg-white rounded-xl border border-slate-200 mt-0 pl-3 text-sm focus:outline-blue-400" placeholder="Digite seu email" />
+
+          <label htmlFor="login-input-password" className="text-blue-950 text-base font-semibold mt-6">
+            Senha
+          </label>
+          <input type="password" id="login-input-password" className="w-105 h-12 bg-white rounded-xl border border-slate-200 mt-0 pl-3 text-sm focus:outline-blue-400 " placeholder="Digite sua senha" />
+
+
+          <div className="flex justify-between">
+            <label htmlFor="remember-me" className="flex items-center">
+              <input type="checkbox" id="remember-me" className="size-4 bg-white rounded-[5px] border border-slate-200" />
+              <span className="text-[#70809F] text-sm">Manter conectado</span>
+            </label>
+          </div>
+
+        </form>
+        </div>
+        
+
+      </section>
+    </main>
+  );
+}
